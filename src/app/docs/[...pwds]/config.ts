37337f2e1870cd15894d7f5438109baecb42cf32
waitezhou <waitezhou@tencent.com>
2023-05-31 17:27:57 +0800

    conditional rendering

--- src/app/docs/[...pwds]/config.ts
+++ src/app/docs/[...pwds]/config.ts
@@ -1,7 +1,7 @@
 export const menuItems = [
   {
     key: 'introduction',
-    label: '前言',
+    label: '译者序',
     children: [
       { key: 'how-to-learn-react', label: '如何学习react' },
       { key: 'why-react', label: '为什么要使用react' },
@@ -15,6 +15,7 @@ export const menuItems = [
       { key: 'importing-and-exporting-components', label: '导入和导出组件' } ,
       { key: 'writing-markup-with-jsx', label: '使用JSX编写标签' },
       { key: 'passing-props-to-a-component', label: '向组件传递Props' },
+      { key: 'conditional-rendering', label: '条件渲染' },
     ]
   },
 ]
