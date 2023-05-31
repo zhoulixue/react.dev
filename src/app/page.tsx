1de609720c458b2f1fac11fb39e436061a7e6b63
waitezhou <waitezhou@tencent.com>
2023-05-31 21:11:03 +0800

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+import {useRouter} from 'next/navigation'
+import style from './page.module.scss'
+
 export default function Home() {
+  const router = useRouter()
+  const handleClick = () => {
+    router.push('/docs/introduction/how-to-learn-react')
+  }
   return (
-    <div>main</div>
+    <div className={style.main}>
+      <div className={style.nav} onClick={handleClick}>React文档</div>
+    </div>
   )
 }
